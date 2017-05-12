660cc243680450aa33be88c2904053c36efada21
jlee29 <jlee29@stanford.edu>
2017-05-12 15:54:54 -0700

    first commit

---
+++ projectMain.py
@@ -0,0 +1,52 @@
+import tensorflow as tf
+import numpy as np
+
+def get_CIFAR10_data(num_training=49000, num_validation=1000, num_test=10000):
+    """
+    Load the CIFAR-10 dataset from disk and perform preprocessing to prepare
+    it for the two-layer neural net classifier. These are the same steps as
+    we used for the SVM, but condensed to a single function.  
+    """
+    # Load the raw CIFAR-10 data
+    cifar10_dir = 'cs231n/datasets/cifar-10-batches-py'
+    X_train, y_train, X_test, y_test = load_CIFAR10(cifar10_dir)
+
+    # Subsample the data
+    mask = range(num_training, num_training + num_validation)
+    X_val = X_train[mask]
+    y_val = y_train[mask]
+    mask = range(num_training)
+    X_train = X_train[mask]
+    y_train = y_train[mask]
+    mask = range(num_test)
+    X_test = X_test[mask]
+    y_test = y_test[mask]
+
+    # Normalize the data: subtract the mean image
+    mean_image = np.mean(X_train, axis=0)
+    X_train -= mean_image
+    X_val -= mean_image
+    X_test -= mean_image
+
+    return X_train, y_train, X_val, y_val, X_test, y_test
+    
+def simple_model(X,y):
+    # define our weights (e.g. init_two_layer_convnet)
+    
+    # setup variables
+    Wconv1 = tf.get_variable("Wconv1", shape=[7, 7, 3, 32])
+    bconv1 = tf.get_variable("bconv1", shape=[32])
+    W1 = tf.get_variable("W1", shape=[5408, 10])
+    b1 = tf.get_variable("b1", shape=[10])
+
+    # define our graph (e.g. two_layer_convnet)
+    a1 = tf.nn.conv2d(X, Wconv1, strides=[1,2,2,1], padding='VALID') + bconv1
+    h1 = tf.nn.relu(a1)
+    h1_flat = tf.reshape(h1,[-1,5408])
+    y_out = tf.matmul(h1_flat,W1) + b1
+    return y_out
+
+def main():
+	print("hi")
+if __name__ == '__main__':
+	main()
